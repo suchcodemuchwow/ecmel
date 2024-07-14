82cc58849dd7a1906cc8e363381c673cdf09067c
Cavit Baturalp Gurdin <suchcodemuchwow@users.noreply.github.com>
2024-07-14 10:03:53 +0200

    :zap: Optimize GitHub link and update turbo and dependencies

--- apps/nextjs/src/app/[lang]/(marketing)/page.tsx
+++ apps/nextjs/src/app/[lang]/(marketing)/page.tsx
@@ -69,7 +69,10 @@ export default async function IndexPage({
                   </ShimmerButton>
                 </Link>
 
-                <Link href="https://github.com/suchcodemuchwow/ecmel" target="_blank">
+                <Link
+                  href="https://github.com/suchcodemuchwow/ecmel"
+                  target="_blank"
+                >
                   <div className="flex h-full items-center justify-center">
                     <Icons.GitHub className="mr-2 h-6 w-6" />
                     <span className="text-base font-semibold">
