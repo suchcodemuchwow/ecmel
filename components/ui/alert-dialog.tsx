197aa4d0fdc4b0ace4c459a92cc63d69f64ab84e
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2025-01-14 10:36:16 +0100

    style: Biome fixes

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -1,10 +1,10 @@
-import * as AlertDialogPrimitive from '@rn-primitives/alert-dialog';
-import * as React from 'react';
-import { Platform, StyleSheet, View, type ViewProps } from 'react-native';
-import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
-import { buttonTextVariants, buttonVariants } from '~/components/ui/button';
-import { cn } from '~/lib/utils';
-import { TextClassContext } from '~/components/ui/text';
+import * as AlertDialogPrimitive from "@rn-primitives/alert-dialog";
+import * as React from "react";
+import { Platform, StyleSheet, View, type ViewProps } from "react-native";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import { buttonTextVariants, buttonVariants } from "~/components/ui/button";
+import { TextClassContext } from "~/components/ui/text";
+import { cn } from "~/lib/utils";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
@@ -13,148 +13,172 @@ const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 const AlertDialogPortal = AlertDialogPrimitive.Portal;
 
 const AlertDialogOverlayWeb = React.forwardRef<
-  AlertDialogPrimitive.OverlayRef,
-  AlertDialogPrimitive.OverlayProps
+    AlertDialogPrimitive.OverlayRef,
+    AlertDialogPrimitive.OverlayProps
 >(({ className, ...props }, ref) => {
-  const { open } = AlertDialogPrimitive.useRootContext();
-  return (
-    <AlertDialogPrimitive.Overlay
-      className={cn(
-        'z-50 bg-black/80 flex justify-center items-center p-2 absolute top-0 right-0 bottom-0 left-0',
-        open ? 'web:animate-in web:fade-in-0' : 'web:animate-out web:fade-out-0',
-        className
-      )}
-      {...props}
-      ref={ref}
-    />
-  );
+    const { open } = AlertDialogPrimitive.useRootContext();
+    return (
+        <AlertDialogPrimitive.Overlay
+            className={cn(
+                "absolute top-0 right-0 bottom-0 left-0 z-50 flex items-center justify-center bg-black/80 p-2",
+                open
+                    ? "web:fade-in-0 web:animate-in"
+                    : "web:fade-out-0 web:animate-out",
+                className,
+            )}
+            {...props}
+            ref={ref}
+        />
+    );
 });
 
-AlertDialogOverlayWeb.displayName = 'AlertDialogOverlayWeb';
+AlertDialogOverlayWeb.displayName = "AlertDialogOverlayWeb";
 
 const AlertDialogOverlayNative = React.forwardRef<
-  AlertDialogPrimitive.OverlayRef,
-  AlertDialogPrimitive.OverlayProps
+    AlertDialogPrimitive.OverlayRef,
+    AlertDialogPrimitive.OverlayProps
 >(({ className, children, ...props }, ref) => {
-  return (
-    <AlertDialogPrimitive.Overlay
-      style={StyleSheet.absoluteFill}
-      className={cn('z-50 bg-black/80 flex justify-center items-center p-2', className)}
-      {...props}
-      ref={ref}
-      asChild
-    >
-      <Animated.View entering={FadeIn.duration(150)} exiting={FadeOut.duration(150)}>
-        {children}
-      </Animated.View>
-    </AlertDialogPrimitive.Overlay>
-  );
+    return (
+        <AlertDialogPrimitive.Overlay
+            style={StyleSheet.absoluteFill}
+            className={cn(
+                "z-50 flex items-center justify-center bg-black/80 p-2",
+                className,
+            )}
+            {...props}
+            ref={ref}
+            asChild
+        >
+            <Animated.View
+                entering={FadeIn.duration(150)}
+                exiting={FadeOut.duration(150)}
+            >
+                {children}
+            </Animated.View>
+        </AlertDialogPrimitive.Overlay>
+    );
 });
 
-AlertDialogOverlayNative.displayName = 'AlertDialogOverlayNative';
+AlertDialogOverlayNative.displayName = "AlertDialogOverlayNative";
 
 const AlertDialogOverlay = Platform.select({
-  web: AlertDialogOverlayWeb,
-  default: AlertDialogOverlayNative,
+    web: AlertDialogOverlayWeb,
+    default: AlertDialogOverlayNative,
 });
 
 const AlertDialogContent = React.forwardRef<
-  AlertDialogPrimitive.ContentRef,
-  AlertDialogPrimitive.ContentProps & { portalHost?: string }
+    AlertDialogPrimitive.ContentRef,
+    AlertDialogPrimitive.ContentProps & { portalHost?: string }
 >(({ className, portalHost, ...props }, ref) => {
-  const { open } = AlertDialogPrimitive.useRootContext();
+    const { open } = AlertDialogPrimitive.useRootContext();
 
-  return (
-    <AlertDialogPortal hostName={portalHost}>
-      <AlertDialogOverlay>
-        <AlertDialogPrimitive.Content
-          ref={ref}
-          className={cn(
-            'z-50 max-w-lg gap-4 border border-border bg-background p-6 shadow-lg shadow-foreground/10 web:duration-200 rounded-lg',
-            open
-              ? 'web:animate-in web:fade-in-0 web:zoom-in-95'
-              : 'web:animate-out web:fade-out-0 web:zoom-out-95',
-            className
-          )}
-          {...props}
-        />
-      </AlertDialogOverlay>
-    </AlertDialogPortal>
-  );
+    return (
+        <AlertDialogPortal hostName={portalHost}>
+            <AlertDialogOverlay>
+                <AlertDialogPrimitive.Content
+                    ref={ref}
+                    className={cn(
+                        "z-50 max-w-lg gap-4 rounded-lg border border-border bg-background p-6 shadow-foreground/10 shadow-lg web:duration-200",
+                        open
+                            ? "web:fade-in-0 web:zoom-in-95 web:animate-in"
+                            : "web:fade-out-0 web:zoom-out-95 web:animate-out",
+                        className,
+                    )}
+                    {...props}
+                />
+            </AlertDialogOverlay>
+        </AlertDialogPortal>
+    );
 });
 AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName;
 
 const AlertDialogHeader = ({ className, ...props }: ViewProps) => (
-  <View className={cn('flex flex-col gap-2', className)} {...props} />
+    <View className={cn("flex flex-col gap-2", className)} {...props} />
 );
-AlertDialogHeader.displayName = 'AlertDialogHeader';
+AlertDialogHeader.displayName = "AlertDialogHeader";
 
 const AlertDialogFooter = ({ className, ...props }: ViewProps) => (
-  <View
-    className={cn('flex flex-col-reverse sm:flex-row sm:justify-end gap-2', className)}
-    {...props}
-  />
+    <View
+        className={cn(
+            "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
+            className,
+        )}
+        {...props}
+    />
 );
-AlertDialogFooter.displayName = 'AlertDialogFooter';
+AlertDialogFooter.displayName = "AlertDialogFooter";
 
 const AlertDialogTitle = React.forwardRef<
-  AlertDialogPrimitive.TitleRef,
-  AlertDialogPrimitive.TitleProps
+    AlertDialogPrimitive.TitleRef,
+    AlertDialogPrimitive.TitleProps
 >(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Title
-    ref={ref}
-    className={cn('text-lg native:text-xl text-foreground font-semibold', className)}
-    {...props}
-  />
+    <AlertDialogPrimitive.Title
+        ref={ref}
+        className={cn(
+            "font-semibold native:text-xl text-foreground text-lg",
+            className,
+        )}
+        {...props}
+    />
 ));
 AlertDialogTitle.displayName = AlertDialogPrimitive.Title.displayName;
 
 const AlertDialogDescription = React.forwardRef<
-  AlertDialogPrimitive.DescriptionRef,
-  AlertDialogPrimitive.DescriptionProps
+    AlertDialogPrimitive.DescriptionRef,
+    AlertDialogPrimitive.DescriptionProps
 >(({ className, ...props }, ref) => (
-  <AlertDialogPrimitive.Description
-    ref={ref}
-    className={cn('text-sm native:text-base text-muted-foreground', className)}
-    {...props}
-  />
+    <AlertDialogPrimitive.Description
+        ref={ref}
+        className={cn(
+            "native:text-base text-muted-foreground text-sm",
+            className,
+        )}
+        {...props}
+    />
 ));
-AlertDialogDescription.displayName = AlertDialogPrimitive.Description.displayName;
+AlertDialogDescription.displayName =
+    AlertDialogPrimitive.Description.displayName;
 
 const AlertDialogAction = React.forwardRef<
-  AlertDialogPrimitive.ActionRef,
-  AlertDialogPrimitive.ActionProps
+    AlertDialogPrimitive.ActionRef,
+    AlertDialogPrimitive.ActionProps
 >(({ className, ...props }, ref) => (
-  <TextClassContext.Provider value={buttonTextVariants({ className })}>
-    <AlertDialogPrimitive.Action ref={ref} className={cn(buttonVariants(), className)} {...props} />
-  </TextClassContext.Provider>
+    <TextClassContext.Provider value={buttonTextVariants({ className })}>
+        <AlertDialogPrimitive.Action
+            ref={ref}
+            className={cn(buttonVariants(), className)}
+            {...props}
+        />
+    </TextClassContext.Provider>
 ));
 AlertDialogAction.displayName = AlertDialogPrimitive.Action.displayName;
 
 const AlertDialogCancel = React.forwardRef<
-  AlertDialogPrimitive.CancelRef,
-  AlertDialogPrimitive.CancelProps
+    AlertDialogPrimitive.CancelRef,
+    AlertDialogPrimitive.CancelProps
 >(({ className, ...props }, ref) => (
-  <TextClassContext.Provider value={buttonTextVariants({ className, variant: 'outline' })}>
-    <AlertDialogPrimitive.Cancel
-      ref={ref}
-      className={cn(buttonVariants({ variant: 'outline', className }))}
-      {...props}
-    />
-  </TextClassContext.Provider>
+    <TextClassContext.Provider
+        value={buttonTextVariants({ className, variant: "outline" })}
+    >
+        <AlertDialogPrimitive.Cancel
+            ref={ref}
+            className={cn(buttonVariants({ variant: "outline", className }))}
+            {...props}
+        />
+    </TextClassContext.Provider>
 ));
 AlertDialogCancel.displayName = AlertDialogPrimitive.Cancel.displayName;
 
 export {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
-  AlertDialogPortal,
-  AlertDialogTitle,
-  AlertDialogTrigger,
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    AlertDialogPortal,
+    AlertDialogTitle,
+    AlertDialogTrigger,
 };
--- components/ui/context-menu.tsx
+++ components/ui/context-menu.tsx
@@ -1,20 +1,20 @@
-import * as ContextMenuPrimitive from '@rn-primitives/context-menu';
-import * as React from 'react';
+import * as ContextMenuPrimitive from "@rn-primitives/context-menu";
+import * as React from "react";
 import {
-  Platform,
-  type StyleProp,
-  StyleSheet,
-  Text,
-  type TextProps,
-  View,
-  type ViewStyle,
-} from 'react-native';
-import { Check } from '~/lib/icons/Check';
-import { ChevronDown } from '~/lib/icons/ChevronDown';
-import { ChevronRight } from '~/lib/icons/ChevronRight';
-import { ChevronUp } from '~/lib/icons/ChevronUp';
-import { cn } from '~/lib/utils';
-import { TextClassContext } from '~/components/ui/text';
+    Platform,
+    type StyleProp,
+    StyleSheet,
+    Text,
+    type TextProps,
+    View,
+    type ViewStyle,
+} from "react-native";
+import { TextClassContext } from "~/components/ui/text";
+import { Check } from "~/lib/icons/Check";
+import { ChevronDown } from "~/lib/icons/ChevronDown";
+import { ChevronRight } from "~/lib/icons/ChevronRight";
+import { ChevronUp } from "~/lib/icons/ChevronUp";
+import { cn } from "~/lib/utils";
 
 const ContextMenu = ContextMenuPrimitive.Root;
 const ContextMenuTrigger = ContextMenuPrimitive.Trigger;
@@ -23,223 +23,232 @@ const ContextMenuSub = ContextMenuPrimitive.Sub;
 const ContextMenuRadioGroup = ContextMenuPrimitive.RadioGroup;
 
 const ContextMenuSubTrigger = React.forwardRef<
-  ContextMenuPrimitive.SubTriggerRef,
-  ContextMenuPrimitive.SubTriggerProps & {
-    inset?: boolean;
-  }
+    ContextMenuPrimitive.SubTriggerRef,
+    ContextMenuPrimitive.SubTriggerProps & {
+        inset?: boolean;
+    }
 >(({ className, inset, children, ...props }, ref) => {
-  const { open } = ContextMenuPrimitive.useSubContext();
-  const Icon = Platform.OS === 'web' ? ChevronRight : open ? ChevronUp : ChevronDown;
-  return (
-    <TextClassContext.Provider
-      value={cn(
-        'select-none text-sm native:text-lg text-primary',
-        open && 'native:text-accent-foreground'
-      )}
-    >
-      <ContextMenuPrimitive.SubTrigger
-        ref={ref}
-        className={cn(
-          'flex flex-row web:cursor-default web:select-none items-center gap-2 web:focus:bg-accent active:bg-accent web:hover:bg-accent rounded-sm px-2 py-1.5 native:py-2 web:outline-none',
-          open && 'bg-accent',
-          inset && 'pl-8',
-          className
-        )}
-        {...props}
-      >
-        <>{children}</>
-        <Icon size={18} className='ml-auto text-foreground' />
-      </ContextMenuPrimitive.SubTrigger>
-    </TextClassContext.Provider>
-  );
+    const { open } = ContextMenuPrimitive.useSubContext();
+    const Icon =
+        Platform.OS === "web" ? ChevronRight : open ? ChevronUp : ChevronDown;
+    return (
+        <TextClassContext.Provider
+            value={cn(
+                "select-none native:text-lg text-primary text-sm",
+                open && "native:text-accent-foreground",
+            )}
+        >
+            <ContextMenuPrimitive.SubTrigger
+                ref={ref}
+                className={cn(
+                    "flex web:cursor-default web:select-none flex-row items-center gap-2 rounded-sm px-2 native:py-2 py-1.5 web:outline-none web:hover:bg-accent web:focus:bg-accent active:bg-accent",
+                    open && "bg-accent",
+                    inset && "pl-8",
+                    className,
+                )}
+                {...props}
+            >
+                {children}
+                <Icon size={18} className="ml-auto text-foreground" />
+            </ContextMenuPrimitive.SubTrigger>
+        </TextClassContext.Provider>
+    );
 });
 ContextMenuSubTrigger.displayName = ContextMenuPrimitive.SubTrigger.displayName;
 
 const ContextMenuSubContent = React.forwardRef<
-  ContextMenuPrimitive.SubContentRef,
-  ContextMenuPrimitive.SubContentProps
+    ContextMenuPrimitive.SubContentRef,
+    ContextMenuPrimitive.SubContentProps
 >(({ className, ...props }, ref) => {
-  const { open } = ContextMenuPrimitive.useSubContext();
-  return (
-    <ContextMenuPrimitive.SubContent
-      ref={ref}
-      className={cn(
-        'z-50 min-w-[8rem] overflow-hidden rounded-md border mt-1 border-border bg-popover p-1 shadow-md shadow-foreground/5 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-        open
-          ? 'web:animate-in web:fade-in-0 web:zoom-in-95'
-          : 'web:animate-out web:fade-out-0 web:zoom-out',
-        className
-      )}
-      {...props}
-    />
-  );
+    const { open } = ContextMenuPrimitive.useSubContext();
+    return (
+        <ContextMenuPrimitive.SubContent
+            ref={ref}
+            className={cn(
+                "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 mt-1 min-w-[8rem] overflow-hidden rounded-md border border-border bg-popover p-1 shadow-foreground/5 shadow-md",
+                open
+                    ? "web:fade-in-0 web:zoom-in-95 web:animate-in"
+                    : "web:fade-out-0 web:zoom-out web:animate-out",
+                className,
+            )}
+            {...props}
+        />
+    );
 });
 ContextMenuSubContent.displayName = ContextMenuPrimitive.SubContent.displayName;
 
 const ContextMenuContent = React.forwardRef<
-  ContextMenuPrimitive.ContentRef,
-  ContextMenuPrimitive.ContentProps & {
-    overlayStyle?: StyleProp<ViewStyle>;
-    overlayClassName?: string;
-    portalHost?: string;
-  }
->(({ className, overlayClassName, overlayStyle, portalHost, ...props }, ref) => {
-  const { open } = ContextMenuPrimitive.useRootContext();
-  return (
-    <ContextMenuPrimitive.Portal hostName={portalHost}>
-      <ContextMenuPrimitive.Overlay
-        style={
-          overlayStyle
-            ? StyleSheet.flatten([
-                Platform.OS !== 'web' ? StyleSheet.absoluteFill : undefined,
-                overlayStyle,
-              ])
-            : Platform.OS !== 'web'
-            ? StyleSheet.absoluteFill
-            : undefined
-        }
-        className={overlayClassName}
-      >
-        <ContextMenuPrimitive.Content
-          ref={ref}
-          className={cn(
-            'z-50 min-w-[8rem] overflow-hidden rounded-md border border-border bg-popover p-1 shadow-md shadow-foreground/5 web:data-[side=bottom]:slide-in-from-top-2 web:data-[side=left]:slide-in-from-right-2 web:data-[side=right]:slide-in-from-left-2 web:data-[side=top]:slide-in-from-bottom-2',
-            open
-              ? 'web:animate-in web:fade-in-0 web:zoom-in-95'
-              : 'web:animate-out web:fade-out-0 web:zoom-out-95',
-            className
-          )}
-          {...props}
-        />
-      </ContextMenuPrimitive.Overlay>
-    </ContextMenuPrimitive.Portal>
-  );
-});
+    ContextMenuPrimitive.ContentRef,
+    ContextMenuPrimitive.ContentProps & {
+        overlayStyle?: StyleProp<ViewStyle>;
+        overlayClassName?: string;
+        portalHost?: string;
+    }
+>(
+    (
+        { className, overlayClassName, overlayStyle, portalHost, ...props },
+        ref,
+    ) => {
+        const { open } = ContextMenuPrimitive.useRootContext();
+        return (
+            <ContextMenuPrimitive.Portal hostName={portalHost}>
+                <ContextMenuPrimitive.Overlay
+                    style={
+                        overlayStyle
+                            ? StyleSheet.flatten([
+                                  Platform.OS !== "web"
+                                      ? StyleSheet.absoluteFill
+                                      : undefined,
+                                  overlayStyle,
+                              ])
+                            : Platform.OS !== "web"
+                              ? StyleSheet.absoluteFill
+                              : undefined
+                    }
+                    className={overlayClassName}
+                >
+                    <ContextMenuPrimitive.Content
+                        ref={ref}
+                        className={cn(
+                            "web:data-[side=bottom]:slide-in-from-top-2 web:data-[side=left]:slide-in-from-right-2 web:data-[side=right]:slide-in-from-left-2 web:data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border border-border bg-popover p-1 shadow-foreground/5 shadow-md",
+                            open
+                                ? "web:fade-in-0 web:zoom-in-95 web:animate-in"
+                                : "web:fade-out-0 web:zoom-out-95 web:animate-out",
+                            className,
+                        )}
+                        {...props}
+                    />
+                </ContextMenuPrimitive.Overlay>
+            </ContextMenuPrimitive.Portal>
+        );
+    },
+);
 ContextMenuContent.displayName = ContextMenuPrimitive.Content.displayName;
 
 const ContextMenuItem = React.forwardRef<
-  ContextMenuPrimitive.ItemRef,
-  ContextMenuPrimitive.ItemProps & {
-    inset?: boolean;
-  }
+    ContextMenuPrimitive.ItemRef,
+    ContextMenuPrimitive.ItemProps & {
+        inset?: boolean;
+    }
 >(({ className, inset, ...props }, ref) => (
-  <TextClassContext.Provider value='select-none text-sm native:text-lg text-popover-foreground web:group-focus:text-accent-foreground'>
-    <ContextMenuPrimitive.Item
-      ref={ref}
-      className={cn(
-        'relative flex flex-row web:cursor-default items-center gap-2 rounded-sm px-2 py-1.5 native:py-2 web:outline-none web:focus:bg-accent active:bg-accent web:hover:bg-accent group',
-        inset && 'pl-8',
-        props.disabled && 'opacity-50 web:pointer-events-none',
-        className
-      )}
-      {...props}
-    />
-  </TextClassContext.Provider>
+    <TextClassContext.Provider value="select-none text-sm native:text-lg text-popover-foreground web:group-focus:text-accent-foreground">
+        <ContextMenuPrimitive.Item
+            ref={ref}
+            className={cn(
+                "group relative flex web:cursor-default flex-row items-center gap-2 rounded-sm px-2 native:py-2 py-1.5 web:outline-none web:hover:bg-accent web:focus:bg-accent active:bg-accent",
+                inset && "pl-8",
+                props.disabled && "web:pointer-events-none opacity-50",
+                className,
+            )}
+            {...props}
+        />
+    </TextClassContext.Provider>
 ));
 ContextMenuItem.displayName = ContextMenuPrimitive.Item.displayName;
 
 const ContextMenuCheckboxItem = React.forwardRef<
-  ContextMenuPrimitive.CheckboxItemRef,
-  ContextMenuPrimitive.CheckboxItemProps
+    ContextMenuPrimitive.CheckboxItemRef,
+    ContextMenuPrimitive.CheckboxItemProps
 >(({ className, children, ...props }, ref) => (
-  <ContextMenuPrimitive.CheckboxItem
-    ref={ref}
-    className={cn(
-      'relative flex flex-row web:cursor-default items-center web:group rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
-      props.disabled && 'web:pointer-events-none opacity-50',
-      className
-    )}
-    {...props}
-  >
-    <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
-      <ContextMenuPrimitive.ItemIndicator>
-        <Check size={14} strokeWidth={3} className='text-foreground' />
-      </ContextMenuPrimitive.ItemIndicator>
-    </View>
-    <>{children}</>
-  </ContextMenuPrimitive.CheckboxItem>
+    <ContextMenuPrimitive.CheckboxItem
+        ref={ref}
+        className={cn(
+            "web:group relative flex web:cursor-default flex-row items-center rounded-sm native:py-2 py-1.5 pr-2 pl-8 web:outline-none web:focus:bg-accent active:bg-accent",
+            props.disabled && "web:pointer-events-none opacity-50",
+            className,
+        )}
+        {...props}
+    >
+        <View className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+            <ContextMenuPrimitive.ItemIndicator>
+                <Check size={14} strokeWidth={3} className="text-foreground" />
+            </ContextMenuPrimitive.ItemIndicator>
+        </View>
+        {children}
+    </ContextMenuPrimitive.CheckboxItem>
 ));
-ContextMenuCheckboxItem.displayName = ContextMenuPrimitive.CheckboxItem.displayName;
+ContextMenuCheckboxItem.displayName =
+    ContextMenuPrimitive.CheckboxItem.displayName;
 
 const ContextMenuRadioItem = React.forwardRef<
-  ContextMenuPrimitive.RadioItemRef,
-  ContextMenuPrimitive.RadioItemProps
+    ContextMenuPrimitive.RadioItemRef,
+    ContextMenuPrimitive.RadioItemProps
 >(({ className, children, ...props }, ref) => (
-  <ContextMenuPrimitive.RadioItem
-    ref={ref}
-    className={cn(
-      'relative flex flex-row web:cursor-default web:group items-center rounded-sm py-1.5 native:py-2 pl-8 pr-2 web:outline-none web:focus:bg-accent active:bg-accent',
-      props.disabled && 'web:pointer-events-none opacity-50',
-      className
-    )}
-    {...props}
-  >
-    <View className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
-      <ContextMenuPrimitive.ItemIndicator>
-        <View className='bg-foreground h-2 w-2 rounded-full' />
-      </ContextMenuPrimitive.ItemIndicator>
-    </View>
-    <>{children}</>
-  </ContextMenuPrimitive.RadioItem>
+    <ContextMenuPrimitive.RadioItem
+        ref={ref}
+        className={cn(
+            "web:group relative flex web:cursor-default flex-row items-center rounded-sm native:py-2 py-1.5 pr-2 pl-8 web:outline-none web:focus:bg-accent active:bg-accent",
+            props.disabled && "web:pointer-events-none opacity-50",
+            className,
+        )}
+        {...props}
+    >
+        <View className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+            <ContextMenuPrimitive.ItemIndicator>
+                <View className="h-2 w-2 rounded-full bg-foreground" />
+            </ContextMenuPrimitive.ItemIndicator>
+        </View>
+        {children}
+    </ContextMenuPrimitive.RadioItem>
 ));
 ContextMenuRadioItem.displayName = ContextMenuPrimitive.RadioItem.displayName;
 
 const ContextMenuLabel = React.forwardRef<
-  ContextMenuPrimitive.LabelRef,
-  ContextMenuPrimitive.LabelProps & {
-    inset?: boolean;
-  }
+    ContextMenuPrimitive.LabelRef,
+    ContextMenuPrimitive.LabelProps & {
+        inset?: boolean;
+    }
 >(({ className, inset, ...props }, ref) => (
-  <ContextMenuPrimitive.Label
-    ref={ref}
-    className={cn(
-      'px-2 py-1.5 text-sm native:text-base font-semibold text-foreground web:cursor-default',
-      inset && 'pl-8',
-      className
-    )}
-    {...props}
-  />
+    <ContextMenuPrimitive.Label
+        ref={ref}
+        className={cn(
+            "web:cursor-default px-2 py-1.5 font-semibold native:text-base text-foreground text-sm",
+            inset && "pl-8",
+            className,
+        )}
+        {...props}
+    />
 ));
 ContextMenuLabel.displayName = ContextMenuPrimitive.Label.displayName;
 
 const ContextMenuSeparator = React.forwardRef<
-  ContextMenuPrimitive.SeparatorRef,
-  ContextMenuPrimitive.SeparatorProps
+    ContextMenuPrimitive.SeparatorRef,
+    ContextMenuPrimitive.SeparatorProps
 >(({ className, ...props }, ref) => (
-  <ContextMenuPrimitive.Separator
-    ref={ref}
-    className={cn('-mx-1 my-1 h-px bg-border', className)}
-    {...props}
-  />
+    <ContextMenuPrimitive.Separator
+        ref={ref}
+        className={cn("-mx-1 my-1 h-px bg-border", className)}
+        {...props}
+    />
 ));
 ContextMenuSeparator.displayName = ContextMenuPrimitive.Separator.displayName;
 
 const ContextMenuShortcut = ({ className, ...props }: TextProps) => {
-  return (
-    <Text
-      className={cn(
-        'ml-auto text-xs native:text-sm tracking-widest text-muted-foreground',
-        className
-      )}
-      {...props}
-    />
-  );
+    return (
+        <Text
+            className={cn(
+                "ml-auto native:text-sm text-muted-foreground text-xs tracking-widest",
+                className,
+            )}
+            {...props}
+        />
+    );
 };
-ContextMenuShortcut.displayName = 'ContextMenuShortcut';
+ContextMenuShortcut.displayName = "ContextMenuShortcut";
 
 export {
-  ContextMenu,
-  ContextMenuCheckboxItem,
-  ContextMenuContent,
-  ContextMenuGroup,
-  ContextMenuItem,
-  ContextMenuLabel,
-  ContextMenuRadioGroup,
-  ContextMenuRadioItem,
-  ContextMenuSeparator,
-  ContextMenuShortcut,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuGroup,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
 };
